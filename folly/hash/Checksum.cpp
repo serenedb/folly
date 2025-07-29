985f39e3931ff2d5ffb1d0707a264cf8b18f1dac
mbkkt <mbkkt@users.noreply.github.com>
2025-07-29 18:00:44 +0000

    Allow to use crc32 (but not crc32c, because abseil version can be used)

--- folly/hash/Checksum.cpp
+++ folly/hash/Checksum.cpp
@@ -83,9 +83,10 @@ bool crc32c_hw_supported_sse42() {
 }
 
 bool crc32c_hw_supported_avx512() {
-  static folly::CpuId id;
-  static bool supported = id.avx512vl() && !detail::hasTrapOnAvx512();
-  return supported;
+  // static folly::CpuId id;
+  // static bool supported = id.avx512vl() && !detail::hasTrapOnAvx512();
+  // return supported;
+  return false;
 }
 
 bool crc32_hw_supported() {
@@ -214,6 +215,7 @@ uint32_t crc32_sw(
 
 } // namespace detail
 
+/*
 uint32_t crc32c(const uint8_t* data, size_t nbytes, uint32_t startingChecksum) {
 #if defined(FOLLY_ENABLE_AVX512_CRC32C_V8S3X4)
   if (detail::crc32c_hw_supported_avx512() && nbytes > 4096) {
@@ -242,6 +244,7 @@ uint32_t crc32c(const uint8_t* data, size_t nbytes, uint32_t startingChecksum) {
     return detail::crc32c_sw(data, nbytes, startingChecksum);
   }
 }
+*/
 
 uint32_t crc32(const uint8_t* data, size_t nbytes, uint32_t startingChecksum) {
 #if FOLLY_AARCH64
@@ -277,6 +280,7 @@ uint32_t crc32_combine(uint32_t crc1, uint32_t crc2, size_t crc2len) {
   }
 }
 
+/*
 uint32_t crc32c_combine(uint32_t crc1, uint32_t crc2, size_t crc2len) {
   // Append up to 32 bits of zeroes in the normal way
   uint8_t data[4] = {0, 0, 0, 0};
@@ -291,5 +295,6 @@ uint32_t crc32c_combine(uint32_t crc1, uint32_t crc2, size_t crc2len) {
     return detail::crc32c_combine_sw(crc1, crc2, crc2len - len);
   }
 }
+*/
 
 } // namespace folly
